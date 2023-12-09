d05fe3df43598e2bf1ec0e56c15439bc0a5be372
mak7on22 <mur-mur-0998@mail.ru>
2023-12-09 16:08:33 +0600

    added a subcategory controller prescribed the logic of deleting adding editing a subcategory and also concluded the category in the index

---
+++ test/Controllers/SubcategoriesController.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using test.Models;
+using test.Models.Categors;
+
+namespace test.Controllers
+{
+    public class SubcategoriesController : Controller
+    {
+        private readonly ProductContext _context;
+
+        public SubcategoriesController(ProductContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Subcategories
+        public async Task<IActionResult> Index()
+        {
+            var subcategories = await _context.Subcategories
+                .Include(s => s.Categorys)
+                .ToListAsync();
+
+            return View(subcategories);
+        }
+
+        // GET: Subcategories/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var subcategory = await _context.Subcategories
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (subcategory == null)
+            {
+                return NotFound();
+            }
+
+            return View(subcategory);
+        }
+
+        // GET: Subcategories/Create
+        public IActionResult Create(int id)
+        {
+            Subcategory subcategory = new() { СategoryId = id, Categorys = _context.Categories.FirstOrDefault(c => c.Id == id) };
+            Category category = _context.Categories.FirstOrDefault(c => c.Id == id);
+            ViewBag.CategoryName = category?.CategoryName ?? "Категория не найдена";
+            return View(subcategory);
+        }
+
+        // POST: Subcategories/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,SubcategoryName,СategoryId")] Subcategory subcategory)
+        {
+            subcategory = new Subcategory { СategoryId = subcategory.Id,SubcategoryName = subcategory.SubcategoryName, Categorys = _context.Categories.FirstOrDefault(c => c.Id == subcategory.Id) };
+            if (ModelState.IsValid)
+            {
+                _context.Add(subcategory);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(subcategory);
+        }
+
+        // GET: Subcategories/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var subcategory = await _context.Subcategories.FindAsync(id);
+            if (subcategory == null)
+            {
+                return NotFound();
+            }
+            return View(subcategory);
+        }
+
+        // POST: Subcategories/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,SubcategoryName,СategoryId")] Subcategory subcategory)
+        {
+            if (id != subcategory.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(subcategory);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SubcategoryExists(subcategory.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(subcategory);
+        }
+
+        // GET: Subcategories/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var subcategory = await _context.Subcategories
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (subcategory == null)
+            {
+                return NotFound();
+            }
+
+            return View(subcategory);
+        }
+
+        // POST: Subcategories/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var subcategory = await _context.Subcategories.FindAsync(id);
+            if (subcategory != null)
+            {
+                _context.Subcategories.Remove(subcategory);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool SubcategoryExists(int id)
+        {
+            return _context.Subcategories.Any(e => e.Id == id);
+        }
+    }
+}
