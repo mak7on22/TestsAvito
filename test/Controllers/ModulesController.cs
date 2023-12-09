7835ac135b4af913490186cbf2d2ee933890bf5d
mak7on22 <mur-mur-0998@mail.ru>
2023-12-09 16:09:56 +0600

    I added logic to the module controller for deleting, creating and editing the module and brought out the hierarchy of subcategories and categories

---
+++ test/Controllers/ModulesController.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using test.Models;
+using test.Models.Categors;
+
+namespace test.Controllers
+{
+    public class ModulesController : Controller
+    {
+        private readonly ProductContext _context;
+
+        public ModulesController(ProductContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Modules
+        public async Task<IActionResult> Index()
+        {
+            var productContext = await _context.Modules
+             .Include(m => m.Subcategories)
+             .ThenInclude(s => s!.Categorys)
+             .ToListAsync();
+            return View(productContext);
+        }
+
+        // GET: Modules/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var @module = await _context.Modules
+                .Include(s => s.Subcategories)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (@module == null)
+            {
+                return NotFound();
+            }
+
+            return View(@module);
+        }
+
+        // GET: Modules/Create
+        public IActionResult Create(int id)
+        {
+            Subcategory subcategory = new() { СategoryId = id, Categorys = _context.Categories.FirstOrDefault(c => c.Id == id) };
+            Category category = _context.Categories.FirstOrDefault(c => c.Id == id);
+            ViewBag.CategoryName = category?.CategoryName ?? "Категория не найдена";
+            ViewData["SubcategoryId"] = new SelectList(_context.Subcategories, "Id", "Id");
+            return View();
+        }
+
+        // POST: Modules/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,ModuleName,SubcategoryId")] Module @module)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(@module);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["SubcategoryId"] = new SelectList(_context.Subcategories, "Id", "Id", @module.SubcategoryId);
+            return View(@module);
+        }
+
+        // GET: Modules/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var @module = await _context.Modules.FindAsync(id);
+            if (@module == null)
+            {
+                return NotFound();
+            }
+            ViewData["SubcategoryId"] = new SelectList(_context.Subcategories, "Id", "Id", @module.SubcategoryId);
+            return View(@module);
+        }
+
+        // POST: Modules/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,ModuleName,SubcategoryId")] Module @module)
+        {
+            if (id != @module.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(@module);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ModuleExists(@module.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["SubcategoryId"] = new SelectList(_context.Subcategories, "Id", "Id", @module.SubcategoryId);
+            return View(@module);
+        }
+
+        // GET: Modules/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var @module = await _context.Modules
+                .Include(s => s.Subcategories)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (@module == null)
+            {
+                return NotFound();
+            }
+
+            return View(@module);
+        }
+
+        // POST: Modules/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var @module = await _context.Modules.FindAsync(id);
+            if (@module != null)
+            {
+                _context.Modules.Remove(@module);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ModuleExists(int id)
+        {
+            return _context.Modules.Any(e => e.Id == id);
+        }
+    }
+}
