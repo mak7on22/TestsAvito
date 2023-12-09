71cd2871e4e477d1dca6005bd8d910b4edb27596
mak7on22 <mur-mur-0998@mail.ru>
2023-12-09 16:05:06 +0600

    the model was added to the database

---
+++ test/Models/ProductContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.ViewEngines;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using test.Models.Categors;
+
+namespace test.Models
+{
+    public class ProductContext : DbContext
+    {
+        public DbSet<Module> Modules { get; set; }
+        public DbSet<Subcategory> Subcategories { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public ProductContext(DbContextOptions<ProductContext> options) : base(options) { }
+    }
+}
